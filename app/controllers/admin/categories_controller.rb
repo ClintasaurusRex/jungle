80eb759cdcf67a6a53fb539af029ba307b207d8c
Clint Hiles <clinthiles1992@gmail.com>
2024-08-22 15:18:57 -0700

    added create category to the admin

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,28 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @products = Product.order(id: :desc).all
+    @categories = Category.all
+  end
+
+  def new
+    @product = Product.new
+    @category = Category.new
+    @categories = Category.all
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+  def category_params
+    params.require(:category).permit(
+      :name,
+
+    )
+  end
+end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,13 @@
 class CategoriesController < ApplicationController
 
+
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
   end
+  def new
+    @category = Category.new
+  end
+
 
 end
