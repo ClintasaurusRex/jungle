a1983e5a305ec05d823ac5688c87db906e9d5844
Clint Hiles <clinthiles1992@gmail.com>
2024-08-27 14:01:05 -0700

    added a cypress home-page test

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,19 @@
+describe("Home Page", () => {
+  it("successfully loads", () => {
+    cy.visit("/");
+  });
+  it("There is 2 products on the page", () => {
+    cy.visit("/");
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("clicks the login-signup button", () => {
+    cy.visit("/");
+    cy.get(".btn.btn-info.login-signup-btn").first().should("be.visible");
+    // Add assertions here based on expected behavior after clicking
+    // For example:
+    // cy.url().should("include", "/login");
+    // or
+    // cy.get(".login-form").should("be.visible");
+  });
+});
